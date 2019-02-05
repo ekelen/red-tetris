f4a11e2c2a2ea44faea13d0cf466bbda7821b7f6
ekelen <ekelen@student.42.us.org>
2019-02-05 11:30:51 +0100

    adds url parsing tests

---
+++ test/parse.js
@@ -0,0 +1,26 @@
+import { configureStore } from './helpers/server'
+import rootReducer from '../src/client/reducers'
+import chai from "chai"
+import { parseURL, SERVER_ENTER_MULTIPLAYER_GAME, START_SINGLE_PLAYER_GAME, URL_INPUT_ERROR } from '../src/client/actions/parse'
+
+chai.should()
+
+describe('URL parser', () => {
+  it('Should start singleplayer game if no hash part of URL', done => {
+    const initialState = {
+      started: false
+    }
+    const store =  configureStore(rootReducer, null, initialState, {
+      START_SINGLE_PLAYER_GAME: ({getState}) => {
+        const state = getState()
+        state.game.started.should.equal(true)
+        done()
+      }
+    })
+    store.dispatch(parseURL('/'))
+  })
+  it('should return url error for a room with non-alphanumeric, !3-20 char username')
+  it('should return url error for a room with non-alphanumeric, !3-20 char roomname')
+  it('should return url error for a room with < 2 or > 5 players')
+  it('should return url error for badly formatted nPlayers specifier')
+})
