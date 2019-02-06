6d7a83854e546dadcb5967b1ce33a67d89c5498c
Leo Simon <l.simon2125@gmail.com>
2019-02-06 17:08:56 +0100

    pieces: generates random pieces for single player (#19)

--- src/client/reducers/currentPiece.js
+++ src/client/reducers/currentPiece.js
@@ -1,20 +1,18 @@
 import { PIECE_FALL, RESET_PIECE, MOVE_PIECE } from '../actions/piece'
+import { shapes } from '../../shapes'
 
-const initialState = {
-  pos: {x: 4, y: 0},
-  shape: [
-    [1,0,0],
-    [1,1,1],
-    [0,0,0]
-  ]
-}
+const getRandomValue = max => Math.round(Math.random() * Math.floor(max))
+const getRandomShape = () => shapes[getRandomValue(shapes.length)]
+const getNewPiece = shape => ({pos: {x: 4, y: 0}, shape})
+
+const initialState = getNewPiece(getRandomShape())
 
 const currentPiece = (state=initialState, action) => {
   switch (action.type) {
     case PIECE_FALL:
       return {...state, pos: {...state.pos, y: state.pos.y + 1}}
     case RESET_PIECE:
-     return action.piece || initialState
+     return action.piece || getNewPiece(getRandomShape()) //TODO: diff resetPiece / newPiece instead of crappy condition
     case MOVE_PIECE:
       return {...state, pos: {...state.pos, x: state.pos.x + action.dir}}
     default:
