6198eef331ff31baefc3428cbe3428c5b1aae45d
ekelen <ekelen@student.42.us.org>
2019-02-12 18:17:47 +0100

    update ghost with placeholder alive/dead classname

--- src/client/components/Ghost.js
+++ src/client/components/Ghost.js
@@ -1,11 +1,8 @@
 import React from 'react'
 import '../styles/board.scss'
 
-// The ghost board (opponents boards)
-
 const Ghost = ({ alive, board, playerName }) => (
-  <div className={'board ghost'}>
-    {/* display board (different design if alive or not) */}
+  <div className={`board ghost ${alive ? 'alive' : 'dead'}`}>
     <span>{`GHOST ${playerName} HERE!`}</span>
   </div>
 )
