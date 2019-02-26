11f9070c6ab626e8dc7da8da433c94b75be2fdb6
failte <lsimon@student.42.fr>
2019-02-26 19:14:15 +0100

    socketMiddleware: tested

--- test/client/socketMiddleWare.js
+++ test/client/socketMiddleWare.js
@@ -8,13 +8,15 @@ import { configureStore } from '../helpers/client'
 import * as server from '../../src/server/index'
 import { ping } from '../../src/client/actions/server';
 import { cloneDeep } from 'lodash'
-import { SERVER_START_GAME, SERVER_PLAYER_DIES } from '../../src/common/constants';
+import { SERVER_START_GAME, SERVER_PLAYER_DIES } from '../../src/common/constants'
+import { socketMiddleWare } from '../../src/client/middleware/socketMiddleWare'
 
 chai.should()
 
 describe('Integration tests: multiplayer game', () => {
   let tetrisServer = null, store = null, socket = null
   const games = []
+  const doNext = () => {}
 
   before(done => {
     server.create(params.serverTest, games)
@@ -27,5 +29,10 @@ describe('Integration tests: multiplayer game', () => {
 
   after(done => {
     tetrisServer ? tetrisServer.stop(done) : done()
+  })
+
+  it('Should return a function if not a server action', () => {
+    const nextHandler = socketMiddleWare(socket)(doNext)({type: 'NOT_A_SERVER_ACTION'})
+    chai.assert.isFunction(nextHandler)
   })
 })
